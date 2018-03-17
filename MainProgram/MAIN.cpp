56d1910658c6068362335dce4be65585b8796386
AFSL Administrator <uwafsl@outlook.com>
2018-03-17 15:55:21 -0700

    Calling MyMath from main program

--- MainProgram/MAIN.cpp
+++ MainProgram/MAIN.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 
+#include "MyMath.h"
+
 using std::cout;
 using std::endl;
 
 int main(void)
 {
 	cout << "Hello World!" << endl;
+
+	//Use an internal library
+	int a = 3;
+	int b = 4;
+	int c = Add(a, b);
+
+	cout << "Calling Add from MyMath.cpp" << endl;
+	cout << a << " + " << b << " = " << c << endl;
 	return(0);
 }
